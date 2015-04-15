ca7c13de346fa8d5df4a4d8db843adacb23c034f
Ben Polinsky <benjamin.polinsky@gmail.com>
2015-04-15 00:24:29 -0400

    bump generator source

--- lib/generators/custom_fields/install/install_generator.rb
+++ lib/generators/custom_fields/install/install_generator.rb
@@ -2,7 +2,7 @@ module CustomFields
   module Generators
     class InstallGenerator < Rails::Generators::Base
       
-      source_root File.expand_path('../../custom_fields ', __FILE__)
+      source_root File.expand_path('../../../custom_fields ', __FILE__)
       
       def create_custom_fields
         generate "model", "custom_field value:text label:text kind:integer multiple:booplean fieldable:references{polymorphic}:index"
